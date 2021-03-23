f1a2edffbd2b76d5b42eae9e7f5bd744f16842c4
leitegs <leitedasilvag@gmail.com>
2021-03-23 09:21:24 -0300

    modificando os nomes das variaveis

--- poc_mock_jest/phone.test.js
+++ poc_mock_jest/phone.test.js
@@ -1,17 +1,19 @@
-const Celular = require('./modules/celular')
+const Phone = require('./modules/celular')
 
 jest.mock('./modules/carregador.js', () => {
   return jest.fn().mockImplementation(() => {
     return {
-      sendEnergy: jest.fn().mockReturnValue(true)
+      sendEnergy: jest.fn().mockReturnValue(70)
     }
   })
 })
 
 describe('Poc mock jest', () => {  
   it('Should return charge', async () => {
-    const celular = new Celular()
-    const response = await celular.charge()
+    const phone = new Phone()
+    const response = await phone.charge()
+
+    console.log(response)
     
     expect(response).not.toBeNull()
   })
